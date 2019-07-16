0fe269150751cf1bd8ca6f5ee827ae4921c45e3c
Sergiy Mohurenko <sergiy.mohurenko@gmail.com>
2019-07-16 09:41:02 +0300

    graphql

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlHttp = require('express-graphql');
 
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolve = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 const app = express();
 const uuidv4 = require('uuid/v4')
 const fileStorage = multer.diskStorage({
@@ -38,11 +42,47 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated!');
+    }
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided!' });
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res 
+        .status(201)
+        .json({ message: 'File stored.', filePath: req.file.path.replace("\\" ,"/") });
+});
+
+
+app.use(
+    '/graphql',
+    graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolve,
+        graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data }
+        }
+    })
+);
 
 app.use((error, req, res, next) => {
     console.log(error);
@@ -57,10 +97,8 @@ mongoose
         'mongodb+srv://sergiymokhurenko:Kp.XaKgGKdt3SZ9@cluster0-hdsn0.mongodb.net/messages', { useNewUrlParser: true }
     )
     .then(result => {
-        const server = app.listen(8080);
-        const io = require('./socket').init(server);
-        io.on('connection', socket => {
-            console.log('Client connected.');
-        })  
+        app.listen(8080);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)
+);
+
